0ad429cc12db2bc1a15ffd15b411b0bea8f307e0
Ashish Tyagi <ashisht@dewsolutions.in>
2020-10-06 12:04:14 +0530

    Initial Commit

---
+++ InfiniteListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfiniteListSwiftUI
+//
+//  Created by Ashish Tyagi on 06/10/20.
+//  Copyright © 2020 Ashish Tyagi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
